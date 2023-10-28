0f8e8a9e86e9614640667e6d7deb72dea51c52f8
peanutChowder <peanutchowder@gmail.com>
2023-10-28 17:10:58 -0600

    Added first draft of summarizer page

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import openai
+
+openai.api_key = ""
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/summarize')
+def my_form():
+    return render_template('summariser-page.html')
+
+@app.route('/summarize', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"Summarize the following for me: {text}"}])
+
+    return completion.choices[0].message.content

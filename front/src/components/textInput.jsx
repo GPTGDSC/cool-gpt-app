67e114475f162d3a7ba90474cd9282984abd013b
Jacob Feng <peanutChowder@gmail.com>
2023-11-14 23:35:01 -0700

    Initial commit

---
+++ front/src/components/textInput.jsx
@@ -0,0 +1,12 @@
+const TextInput = () => {
+    return (
+        <div>
+            <input 
+                placeholder="Enter text here..."
+                name="textInput"
+            />
+        </div>
+    )
+}
+
+export default TextInput;

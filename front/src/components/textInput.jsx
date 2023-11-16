777efe94a304fadb9c41b99b40635dd1dec6685f
Jacob Feng <peanutChowder@gmail.com>
2023-11-15 18:50:50 -0700

    Removed template, added input textarea

--- front/src/components/textInput.jsx
+++ front/src/components/textInput.jsx
@@ -1,10 +1,14 @@
 const TextInput = () => {
+    const handleChange = (event) => {
+        console.log(`${event.target.value}!!!!`)
+    }
     return (
         <div>
-            <input 
+            <textarea 
                 placeholder="Enter text here..."
-                name="textInput"
-            />
+                onChange={handleChange}
+            ></textarea>
+                
         </div>
     )
 }

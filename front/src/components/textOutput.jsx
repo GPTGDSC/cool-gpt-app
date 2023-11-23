5fdd91cd7fd77b22360824a151750ea770499eb7
Jacob Feng <peanutChowder@gmail.com>
2023-11-22 23:02:31 -0700

    Initial commit

---
+++ front/src/components/textOutput.jsx
@@ -0,0 +1,11 @@
+const TextOutput = () => {
+    return (
+        <div>
+            <div id='text-output'>
+                placeholder
+            </div>
+        </div>
+    )
+}
+
+export default TextOutput

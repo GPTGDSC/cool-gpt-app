e5a8fc511068f8222a0914d031525246818a8958
Jacob Feng <peanutChowder@gmail.com>
2023-11-24 16:18:10 -0700

    Refactor header into sidebar, fixed indentation

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     if (busy) {
       return
     }
-    
+
     setbusy(true);
     const response = await api.summarizeText(inputText)
 
@@ -23,38 +23,38 @@ function App() {
     setbusy(false);
   }
 
-  const style = busy ? {} : {display: 'none'} 
+  const style = busy ? {} : { display: 'none' }
 
   return (
 
-      <div id='main'>
-        <div id='header-container'>
+    <div id='main'>
+      <div id='main-content-container'>
+        <div id="sidebar">
           <h1>Thoughtful summarizer</h1>
           <p>Summarize your files</p>
         </div>
-        <div id='main-content-container'>
-          <div id="paste-summarize-container">
+        <div id="paste-summarize-container">
           <div className="upload-btn-wrapper">
             <button className="upload-btn">Upload a file</button>
             <input type="file" name="myfile" />
           </div>
-            <p>...or paste your text here</p>
-            <TextInput setInputText={setInputText}/>
-            <button 
-              className='button-5'
-              id='summarize-button'
-              onClick={handleBtnClick}
-            >Summarize</button>
-          </div>
-          <div 
-            className="lds-roller"
-            style={style}
-          ><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-          <div id='output-summarize-container'>
-            <TextOutput outputText={outputText}/>
-          </div>
+          <TextInput setInputText={setInputText} />
+          <button
+            className='button-5'
+            id='summarize-button'
+            onClick={handleBtnClick}
+          >Summarize</button>
+        </div>
+        <div
+          className="lds-roller"
+          style={style}
+        ><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+
+        <div id='output-summarize-container'>
+          <TextOutput outputText={outputText} />
         </div>
       </div>
+    </div>
   )
 }
 

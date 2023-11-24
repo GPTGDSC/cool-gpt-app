5261acd5a90aecc6b3a033bcf87a80e71719878b
Jacob Feng <peanutChowder@gmail.com>
2023-11-23 22:42:58 -0700

    Added prop typing

--- front/src/components/textOutput.jsx
+++ front/src/components/textOutput.jsx
@@ -1,11 +1,18 @@
-const TextOutput = () => {
+import PropTypes from 'prop-types'
+
+const TextOutput = ({ outputText }) => {
+
     return (
         <div>
             <div id='text-output'>
-                <p>Output goes here</p>
+                <p>{outputText}</p>
             </div>
         </div>
     )
+}
+
+TextOutput.propTypes = {
+    outputText: PropTypes.string
 }
 
 export default TextOutput

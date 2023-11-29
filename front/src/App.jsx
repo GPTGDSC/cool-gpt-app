c29833947497f825ebdcac39ee23bed74da30fae
Jacob Feng <peanutChowder@gmail.com>
2023-11-29 12:20:23 -0700

    Remove redundant container

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -28,33 +28,31 @@ function App() {
   return (
 
     <div id='main'>
-      <div id='main-content-container'>
-        <div id="sidebar">
-          <h1>Thoughtful summarizer</h1>
-          <p>Summarize your files</p>
-        </div>
-        <div id="paste-summarize-container">
-          <div className="upload-btn-wrapper">
-            <button className="upload-btn">Upload a file</button>
-            <input type="file" name="myfile" />
-          </div>
-          <TextInput setInputText={setInputText} />
-          <button
-            className='button-5'
-            id='summarize-button'
-            onClick={handleBtnClick}
-          >Summarize</button>
-        </div>
-        <div
-          className="lds-roller"
-          style={style}
-        ><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-
-        <div id='output-summarize-container'>
-          <TextOutput outputText={outputText} />
-        </div>
+      <div id="sidebar">
+        <h1>Quick sum</h1>
+        <p>Summarize your files</p>
       </div>
-    </div>
+      <div id="paste-summarize-container">
+        <div className="upload-btn-wrapper">
+          <button className="upload-btn">Upload a file</button>
+          <input type="file" name="myfile" />
+        </div>
+        <TextInput setInputText={setInputText} />
+        <button
+          className='button-5'
+          id='summarize-button'
+          onClick={handleBtnClick}
+        >Summarize</button>
+      </div>
+      <div
+        className="lds-roller"
+        style={style}
+      ><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+
+      <div id='output-summarize-container'>
+        <TextOutput outputText={outputText} />
+      </div>
+    </div>  
   )
 }
 

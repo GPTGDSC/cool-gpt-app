a3238547865346d659d0970dcb38be6c7f6518bc
Jacob Feng <peanutChowder@gmail.com>
2023-11-30 12:16:18 -0700

    Added proptype and state setter for inputtext

--- front/src/components/fileUpload.jsx
+++ front/src/components/fileUpload.jsx
@@ -1,14 +1,15 @@
+import PropTypes from 'prop-types'
 import api from '../services/api'
 
-const FileUpload = () => {
+const FileUpload = ({ setInputText }) => {
     const handleFileChange = async (event) => {
         const file = event.target.files[0];
-    
         if (file == null) {
           return;
         }
         
-        api.uploadFile(file)
+        const text = await api.uploadFile(file)
+        await setInputText(text)
       }
     
     return (
@@ -18,6 +19,10 @@ const FileUpload = () => {
             onChange={handleFileChange}
         />
     )
+}
+
+FileUpload.propTypes = {
+  setInputText: PropTypes.func.isRequired
 }
 
 export default FileUpload;
